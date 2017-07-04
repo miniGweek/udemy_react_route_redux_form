3aaac76bbc9f52d8ba11fd685c2f60e4e57530a3
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-07-04 11:46:26 +1000

    renaming js files to jsx to assist vscode syntax check to work correctly Adding first simple routing.

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,30 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
+import {BrowserRouter, Route} from 'react-router-dom'
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+const Hello = ()=>{
+  return(
+    <div> This is a Hello Component</div>
+  );
+}
+
+const Greetings = ()=>{
+  return(
+    <div> This is a Greetings Component</div>
+  );
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+    <div>
+      <Route path="/hello" component={Hello}/>
+      <Route path="/greetings" component={Greetings}/>
+    </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
