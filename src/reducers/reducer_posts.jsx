e7d4c57681d3e81ff3777610c748d43a462a3ff3
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-07-04 14:34:19 +1000

    returning a different state object from reducer, instead of array of posts. using lodash mapkeys

--- src/reducers/reducer_posts.jsx
+++ src/reducers/reducer_posts.jsx
@@ -1,9 +1,10 @@
 import {FETCH_POSTS} from '../actions'
+import _ from 'lodash'
 
 export default function posts(state = {} , action){
     switch(action.type){
         case FETCH_POSTS:
-            console.log(action.payload.data);
+           return _.mapKeys(action.payload.data, post => post.id) ;
         default:
             return {};
     }
