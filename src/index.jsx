30c237550681188bfd8bde8fe57b9d576f3d54d4
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2017-07-04 13:15:25 +1000

    adding a simple route to container posts_index ( a component/container from external file )

--- src/index.jsx
+++ src/index.jsx
@@ -7,29 +7,18 @@ import App from './components/app';
 import reducers from './reducers';
 import {BrowserRouter, Route} from 'react-router-dom'
 
+import PostsIndex from './containers/posts_index'
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-const Hello = ()=>{
-  return(
-    <div> This is a Hello Component</div>
-  );
-}
-
-const Greetings = ()=>{
-  return(
-    <div> This is a Greetings Component</div>
-  );
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-    <div>
-     <h1>Hello World!</h1> 
-      <Route path="/hello" component={Hello}/>
-      <Route path="/greetings" component={Greetings}/>
-    </div>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
